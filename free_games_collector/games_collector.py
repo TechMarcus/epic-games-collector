d8aedd28f7fd042e838df2c2f565b6f334c38f3d
Mark Vos <ranger.ahilles@gmail.com>
2025-04-22 21:16:30 +0300

    Small fixes

--- free_games_collector/games_collector.py
+++ free_games_collector/games_collector.py
@@ -1,4 +1,5 @@
 from calendar import c
+from encodings.punycode import T
 from seleniumbase import SB
 import time
 
@@ -14,49 +15,58 @@ class EgsAccount:
 
 def multi_collector(accounts):
     for account in accounts:
-        with SB(headless=True) as sb:
+        with SB(uc=True, headless=True) as sb:
             sb.activate_cdp_mode("https://www.epicgames.com/id/login?lang=en-US&redirectUrl=https%3A%2F%2Fstore.epicgames.com%2Fen-US%2F")
 
             sb.cdp.type("//*[@id='email']", account)
             sb.cdp.type("//*[@id='password']", accounts[account])
-            time.sleep(1)
+            sb.sleep(5)
             sb.cdp.mouse_click("button[type='submit']")
-            time.sleep(5)
+            sb.sleep(10)
+            
             collector(sb)
 
 
 def collector(sb):
             try:
                 free_games = sb.convert_xpath_to_css("/html/body/div[1]/div/div/div[4]/main/div[2]/div/div/div/div[2]/div[2]/span[7]/div/div/section/div")
+                sb.sleep(10)
+                sb.cdp.scroll_into_view(free_games)
             except:
                 print("Free games not found by xpath")
-            sb.cdp.scroll_into_view(free_games)
-            sb.sleep(1)
+                
             for i in range(1,5):
                 try:
                     free_game = sb.convert_xpath_to_css(f"/html/body/div[1]/div/div/div[4]/main/div[2]/div/div/div/div[2]/div[2]/span[7]/div/div/section/div/div[{i}]")
+                    sb.sleep(10)
                 except:
-                    # print("Free game not found by xpath")
+                    print("Free game not found by xpath")
                     break
-                sb.cdp.mouse_click(free_game)
-                time.sleep(4)
+                try:
+                    sb.cdp.mouse_click(free_game)
+                except:
+                    sb.sleep(5)
+                    sb.cdp.scroll_into_view(free_games)
+                    sb.cdp.mouse_click(free_game)
 
                 get_button_selector = "button[data-testid='purchase-cta-button']"
+                sb.sleep(5)
 
                 try:
                     sb.cdp.scroll_into_view(get_button_selector)
+                    sb.sleep(10)
                 except:
-                    # print("No get button")
+                    print("No get button")
                     sb.cdp.go_back()
                     continue
 
                 if sb.cdp.get_text(get_button_selector) != "Get":
-                    # print("Not get")
+                    print("Not avalible to get")
                     sb.cdp.go_back()
                     continue
 
                 sb.cdp.mouse_click(get_button_selector)
-                time.sleep(7)
+                sb.sleep(10)
 
                 try:
                     iframe = sb.convert_xpath_to_css("/html/body/div[6]/iframe")
@@ -66,7 +76,7 @@ def collector(sb):
                     print("not logged in")
                     return
 
-                time.sleep(11)
+                sb.sleep(10)
                 sb.cdp.refresh()
                 sb.cdp.go_back()
                 sb.cdp.go_back()
--- free_games_collector/main.py
+++ free_games_collector/main.py
@@ -2,11 +2,12 @@ from games_collector import EgsAccount, multi_collector
 import asyncio
 import os, json
 
-with open("accounts.json", "r") as f:
-    accounts = json.load(f)
 
 def main():
-    test_user = EgsAccount(user=os.getlogin())
+    # test_user = EgsAccount(user=os.getlogin())
+    # test_user.single_collector()
+    with open("accounts.json", "r") as f:
+        accounts = json.load(f)
     multi_collector(accounts)
 
 if __name__ == "__main__":
